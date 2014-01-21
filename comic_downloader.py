e48b79c89c4a45c3885b34c8a487b000c07fd8a4
anarchih <anarchihz@gmail.com>
2014-01-21 21:21:00 +0800

    split all website

--- comic_downloader.py
+++ comic_downloader.py
@@ -7,4 +7,19 @@ org_bytes = fp.read()
 org_str = org_bytes.decode("utf-8",'ignore')
 soup = BeautifulSoup(''.join(org_str))
 tag = soup.find('script').string
-print (tag)
+l = list (tag)
+#print (l[0])
+#l = ['a','b','c','d','/','a','|','b','|','c']
+k = []
+#l = l[1:]
+#print (l[0], l[1])
+string = ''
+for i in l:
+#	l = l[1:]
+#	print (l[0])
+	if l[0] == "/":
+		string = ''.join(l) 
+		k = string.split("|")
+		break;
+	l = l[1:]
+print (k)
