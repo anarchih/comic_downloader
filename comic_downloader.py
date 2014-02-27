c68ea1cf3e416779357d44faedebd06c82d98e92
anarchih <anarchih.tw@gmail.com>
2014-02-27 22:07:41 +0800

    simplified codes

--- comic_downloader.py
+++ comic_downloader.py
@@ -2,27 +2,27 @@ from bs4 import BeautifulSoup
 import urllib.request
 import shutil
 import os
-url = ""
-#url.input()
 url = "http://mh.99770.cc/comic/2779/21390/"
-fp = urllib.request.urlopen(url)
+
+furl =  ["http://58.215.241.39:9728/dm01/","http://58.215.241.39:9728/dm02/","http://58.215.241.39:9728/dm03/","http://58.215.241.206:9728/dm04/","http://58.215.241.39:9728/dm05/","http://58.215.241.39:9728/dm06/","http://58.215.241.39:9728/dm07/","http://58.215.241.39:9728/dm08/","http://58.215.241.206:9728/dm09/","http://58.215.241.39:9728/dm10/","http://58.215.241.39:9728/dm11/","http://58.215.241.206:9728/dm12/","http://58.215.241.39:9728/dm13/","http://173.231.57.238/dm14/","http://58.215.241.206:9728/dm15/","http://142.4.34.102/dm16/"]
+
+class MyOpener(urllib.request.FancyURLopener):
+	version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+myopener = MyOpener()
+fp = myopener.open(url);
+#fp = urllib.request.urlopen(url)
 org_bytes = fp.read()
 org_str = org_bytes.decode("utf-8",'ignore')
 soup = BeautifulSoup(''.join(org_str))
 tag = soup.find('script').string
 l = list (tag)
-#print (l[0])
-#l = ['a','b','c','d','/','a','|','b','|','c']
 k = []
-#l = l[1:]
-#print (l[0], l[1])
 string = ''
 dm = 0
-furl =  ["http://58.215.241.39:9728/dm01/","http://58.215.241.39:9728/dm02/","http://58.215.241.39:9728/dm03/","http://58.215.241.206:9728/dm04/","http://58.215.241.39:9728/dm05/","http://58.215.241.39:9728/dm06/","http://58.215.241.39:9728/dm07/","http://58.215.241.39:9728/dm08/","http://58.215.241.206:9728/dm09/","http://58.215.241.39:9728/dm10/","http://58.215.241.39:9728/dm11/","http://58.215.241.206:9728/dm12/","http://58.215.241.39:9728/dm13/","http://173.231.57.238/dm14/","http://58.215.241.206:9728/dm15/","http://142.4.34.102/dm16/"]
 
 while True:
 	if l[-1].isdigit():
-		dm=int(l[-1])
+		dm = int(l[-1])
 	if l[-1]!='g':
 		l = l[:-1]	
 	else :
@@ -36,9 +36,6 @@ while True:
 		break;
 	l = l[1:]
 #entire_url = furl[dm-1]+k[1]
-class MyOpener(urllib.request.FancyURLopener):
-	version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-myopener = MyOpener()
 file_name = "comic"
 entire_name = ""
 folder_name ="test"
